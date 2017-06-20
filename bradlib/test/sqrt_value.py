2f0f43b6454264c757f819c32c18161588ccde54
bradkeogh <brad.keogh@gmail.com>
2017-06-20 09:41:23 +0100

    added sqt function

---
+++ bradlib/test/sqrt_value.py
@@ -0,0 +1,8 @@
+def sqrt_value(x):
+    import numpy as np
+    y = np.sqrt(x)
+    print('The original value is: ', x)
+    print('The root of value is: ', y)
+    print('-----------------------')
+    print('Scipt complete')
+    return
